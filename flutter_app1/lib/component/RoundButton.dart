848ce36303c8e8ec3fde6a8a1015679c10fd16db
MonkeyLLuffy <706731198@qq.com>
2021-06-27 23:26:33 +0800

    修改手机号码登录页面

--- flutter_app1/lib/component/RoundButton.dart
+++ flutter_app1/lib/component/RoundButton.dart
@@ -6,31 +6,42 @@ class RoundButton extends StatelessWidget {
   String btnText;
   double? textSize = 16;
   double roundAngle = 30;
+  double? width;
+  double? height;
+  EdgeInsetsGeometry? padding;
   var onPressed;
   RoundButton(this.btnText, this.roundAngle,
       {Key? key,
       this.btnBgColor,
       this.btnTextColor,
       this.textSize,
+      this.width,
+      this.height,
+      this.padding,
       this.onPressed})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      onPressed: this.onPressed,
-      style: ButtonStyle(
-          backgroundColor: MaterialStateProperty.all(this.btnBgColor),
+        onPressed: this.onPressed,
+        style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(this.btnBgColor),
 
-          ///圆角
-          shape: MaterialStateProperty.all(RoundedRectangleBorder(
-              side: BorderSide.none,
-              borderRadius:
-                  BorderRadius.all(Radius.circular(this.roundAngle))))),
-      child: Text(
-        this.btnText,
-        style: TextStyle(fontSize: this.textSize, color: this.btnTextColor),
-      ),
-    );
+            ///圆角
+            shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                side: BorderSide.none,
+                borderRadius:
+                    BorderRadius.all(Radius.circular(this.roundAngle))))),
+        child: Container(
+          padding: this.padding,
+          width: this.width,
+          height: this.height,
+          child: Text(
+            this.btnText,
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: this.textSize, color: this.btnTextColor),
+          ),
+        ));
   }
 }
--- flutter_app1/lib/kehuaApp/PhoneLogin.dart
+++ flutter_app1/lib/kehuaApp/PhoneLogin.dart
@@ -1,7 +1,9 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterapp1/component/CircleIcon.dart';
 import 'package:flutterapp1/component/RoundButton.dart';
 import 'package:flutterapp1/component/SizeText.dart';
+import 'package:flutterapp1/kehuaApp/AgreementDialog.dart';
 
 class PhoneLoginPage extends StatelessWidget {
   const PhoneLoginPage({Key? key}) : super(key: key);
@@ -42,11 +44,79 @@ class PhoneLoginPage extends StatelessWidget {
                   height: 50,
                   child: RoundButton("本机号码一键登录", 50.0,
                       btnBgColor: Color(0xffFF295E),
+                      width: 250,
                       btnTextColor: Colors.white,
-                      textSize: 20))
+                      textSize: 20)),
+              SizedBox(height: 10),
+              Container(
+                  height: 50,
+                  child: RoundButton("输入手机号登录", 50.0,
+                      btnBgColor: Color(0xffF8F7FC),
+                      width: 250,
+                      btnTextColor: Color(0xffFF295E),
+                      textSize: 20)),
             ],
           ),
-        )
+        ),
+        Align(
+            alignment: Alignment.bottomLeft,
+            child: new Container(
+              padding: EdgeInsets.all(10),
+              child: new Row(
+                children: [
+                  Checkbox(
+                      value: true,
+                      // checkColor: Color(0xffFF295E),
+                      onChanged: (value1) => {
+                            print("选中了吗？答曰:$value1"),
+                          }),
+                  Expanded(
+                      child: RichText(
+                    //文字居中
+                    textAlign: TextAlign.left,
+                    //文字区域
+                    text: TextSpan(
+                        text: "登录表示同意",
+                        style: TextStyle(color: Colors.grey, fontSize: 18),
+                        children: [
+                          TextSpan(
+                              text: " 用户协议 ",
+                              style: TextStyle(color: Colors.blueAccent),
+                              recognizer: TapGestureRecognizer()
+                                ..onTap = () {
+                                  // todo 跳转其他界面
+                                  print("对话框显示用户协议");
+                                  AgreementDialog(context, "用户协议",
+                                          "这是可话的用户协议XXXXXXXXXX,SSDSDSWE无法实施身高多少时间吗是的加法公式计算框架是")
+                                      .show();
+                                }),
+                          TextSpan(text: " 和 "),
+                          TextSpan(
+                              text: " 隐私政策 ",
+                              style: TextStyle(color: Colors.blueAccent),
+                              recognizer: TapGestureRecognizer()
+                                ..onTap = () {
+                                  // todo 跳转其他界面
+                                  AgreementDialog(context, "隐私政策",
+                                          "这是可话的隐私政策XXXXXXXXXX,SSDSDSWE无法实施身高多少时间吗是的加法公式计算框架是")
+                                      .show();
+                                }),
+                          TextSpan(text: " 以及 "),
+                          TextSpan(
+                              text: " 中国移动认证条款 ",
+                              style: TextStyle(color: Colors.blueAccent),
+                              recognizer: TapGestureRecognizer()
+                                ..onTap = () {
+                                  // todo 跳转其他界面
+                                  AgreementDialog(context, "中国移动认证条款",
+                                          "这是可话的中国移动认证条款XXXXXXXXXX,SSDSDSWE无法实施身高多少时间吗是的加法公式计算框架是")
+                                      .show();
+                                }),
+                        ]),
+                  ))
+                ],
+              ),
+            ))
       ])),
     );
   }
